7cc699c2f5d577c33854427e0043e7296bf2ebeb
Tuomas Pöyry <tuomas@munkkeli.io>
2018-11-15 18:24:00 +0200

    Everything exept Express server done

--- js/main_b.js
+++ js/main_b.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // HTML contains element 'message'. This is used to show the server's response
 // Select it and save it as a variable/object
@@ -13,3 +13,30 @@
 // function ends
 
 // make an event listener which calls upload function when the form is submitted
+
+const upload = event => {
+  event.preventDefault();
+
+  // select input element where type is file
+  const input = document.querySelector('input[type="file"]');
+  // make FormData -object
+  const data = new FormData();
+  // add file to FormData -object.
+  // Note that 'files' is an FileList object. This means that you can upload multiple files.
+  data.append("fileToUpload", input.files[0]);
+  // make an object for settings
+  const settings = {
+    method: "POST",
+    // credentials: 'same-origin', // this might be needed for some servers
+    body: data
+  };
+  // initiate fetch. In this example the server responds with json.
+  // Response could also be text. Then you would use response.text()
+  fetch("someUrl", settings)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+    });
+};
